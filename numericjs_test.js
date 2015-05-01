25f76d42c8f12869a9f08c566e4a5c59f2d5a2d1
Tom Tichy <ttichy@ra.rockwell.com>
2015-05-01 14:44:24 -0400

    changing to clamped splines

--- numericjs_test.js
+++ numericjs_test.js
@@ -1,17 +1,16 @@
 
 var numeric = require('numeric');
-
-// ReSharper disable once WrongExpressionStatement
 "use strict";
 
 /**
  *  INPUTS:
  *
- *  c1 .. initial slope
- *  cn .. final slope
+ *  s0 .. initial slope
+ *  sf .. final slope
  *  
  *  X.... array of x values
  *  Y.... array of y values
+ *  T.....array of types (linear=0, cubic=1)
  *
  *
  * OUTPUT:
@@ -21,22 +20,27 @@ var numeric = require('numeric');
  * 
  */
 
+// the type of cubic interpolation that is used is called 'clamped'
 
-// natural cubic spline can be solved relatively easy with Thompson algorithm (because the matrix is tri-diagonal)
-// however, since we need a more generic solution, just going with the numeric library
 
 
 //-----<INPUTS>---------------------------------------
 
-// start with natural cubic spline
-var c1=0;
-var cn=0;
+var s0=0;
+var sf=0;
 
-var X = [0,1];	//define X points
-var Y = [0,2];	//define Y points
+var X = [0,1,3,4];	//define X points
+var Y = [0,0,2,2];	//define Y points
+var T = [1,1,1];    // type one less than points
 
 //-----</INPUTS>----------------------------------------
 
+// data checks
+if(X.length != Y.length) {
+	throw new Error("Matrices must have the same length");
+}
+if(T.length != X.length-1)
+    throw new Error("T must have one less members than X and Y")
 
 /**
  * [Am populate matrix row]
@@ -46,7 +50,7 @@ var Y = [0,2];	//define Y points
  */
 var Am = function (m, r, h) {
     
-    var hSize = h.length;
+    var hSize = h.length+1;
     if (r > hSize)
         throw new Error("passed row number too large.");
     
@@ -63,32 +67,36 @@ var Am = function (m, r, h) {
     }
 }
 
-var Bm = function (r, h, deltaY) {
-    return 3 * deltaY[r + 1] / h[r + 1] - 3 * deltaY[0] / h[r];
+
+
+var Bm = function (r, d) {
+    //first row?
+    if(r==0)
+        return 6*(d[0]-s0);
+
+    //last row?
+    if(r==d.length+1)
+        return 6*(sf-d[r-1]);
+
+    //all other rows
+    return 6*(d[r]-d[r-1];
 }
 
 
-if(X.length != Y.length) {
-	throw new Error("Matrices must have the same length");
-}
-
-
-
-
-// define and assign h and deltaYj
+// define and assign h and slopes d
 var h=[];
-var deltaYj=[];
+var d=[];
 
 for (var i = 1; i < X.length; i++) {
 	h[i-1]=X[i]-X[i-1];
-	deltaYj[i-1]=Y[i]-Y[i-1];
+	d[i-1]=(Y[i]-Y[i-1])/h[i-1];
 }
 
 // need to have matrices in form AX=B, then can do
 // inv(A)*B=X
 
-// A matrix has X.length - 2 rows
-var rows = X.length - 2;
+
+var rows = X.length;
 var cols = rows;
 
 var A = new Array();
@@ -134,11 +142,11 @@ if(rows>0) {
     C = numeric.dot(Ainv, B);
 
     
-    var C1 = [c1];
-    var CN = [cn];
+    var s0 = [s0];
+    var sf = [sf];
     
-    C.unshift(C1);
-    C.push(CN);
+    C.unshift(s0);
+    C.push(sf);
     
     
     console.log(A);
@@ -171,8 +179,8 @@ if(rows>0) {
     B = [
         [Y[0]],
         [Y[1]],
-        [c1],
-        [cn]
+        [s0],
+        [sf]
     ];
 
     var Ainv = numeric.inv(A);
