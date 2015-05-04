22833608bdb776d6a4621d764e5b14e2bd481784
Tomas Tichy <ttichy@yahoo.com>
2015-05-04 10:14:25 -0400

    clamped spline solution, cubic only

--- cam_conversion.js
+++ cam_conversion.js
@@ -31,7 +31,7 @@ var sf=0;
 
 var X = [0,1,3,4];	//define X points
 var Y = [0,0,2,2];	//define Y points
-var T = [1,1,1];    // type one less than points
+var T = [1,1,1];    // type - linear or cubic ( one less than points)
 
 //-----</INPUTS>----------------------------------------
 
@@ -54,13 +54,22 @@ var Am = function (m, r, h) {
     if (r > hSize)
         throw new Error("passed row number too large.");
     
+    // juggle the h's a bit in order to make handle first and last row
+    var prevH=h[r-1];
+    if(!!!prevH)
+    	prevH=0;
+    var thisH=h[r];
+    if(!!!thisH)
+    	thisH=0;
+
+
     switch (m) {
         case 1:
-            return h[r];
+            return prevH;
         case 2:
-            return 2 * (h[r] + h[r + 1]);
+            return 2 * (prevH+thisH);
         case 3:
-            return h[r + 1];
+            return thisH;
         default:
             throw new Error("only 1,2 or 3 are valid values for m");
 
@@ -75,11 +84,11 @@ var Bm = function (r, d) {
         return 6*(d[0]-s0);
 
     //last row?
-    if(r==d.length+1)
+    if(r==d.length)
         return 6*(sf-d[r-1]);
 
     //all other rows
-    return 6*(d[r]-d[r-1];
+    return 6*(d[r]-d[r-1]);
 }
 
 
@@ -103,91 +112,70 @@ var A = new Array();
 var B = new Array();
 var C = new Array();
 
-if(rows>0) {
-    for (var row = 0; row < rows; row++) {
+    	debugger;
 
-        //create a new row and fill with zeroes
-    
-        A[row] = Array.apply(null, new Array(cols)).map(Number.prototype.valueOf, 0);
-
-    
-        // which column to start in
-        var startCol = row - 1;
-        var stopCol = startCol + 3;
-    
-        //special cases for first and last row
-
-        if (startCol < 0) {
-            stopCol = 2;
-            startCol = 0;
-        }
-
-        if (stopCol > rows)
-            stopCol = startCol + 2;
-
-        if (rows == 1)
-            stopCol = 1;
-
-        for (var col = startCol; col < stopCol; col++) {
-            A[row][col] = Am(col-row+2 , row, h);
-        }
-
-        B[row] = new Array();
-        B[row][0] = Bm(row, h, deltaYj);
+for (var row = 0; row < rows; row++) {
 
 
-    };
+    //create a new row and fill with zeroes
 
-    var Ainv = numeric.inv(A);
-    C = numeric.dot(Ainv, B);
+    A[row] = Array.apply(null, new Array(cols)).map(Number.prototype.valueOf, 0);
 
-    
-    var s0 = [s0];
-    var sf = [sf];
-    
-    C.unshift(s0);
-    C.push(sf);
-    
-    
-    console.log(A);
-    console.log(B);
-    console.log(C);
-    
-    //calculate the rest of coefficients
-    
-    var d = [];
-    var a = [];
-    var b = [];
-    for (var i = 0; i < X.length - 1; i++) {
-        d[i] = (C[i + 1] - C[i]) / (3 * h[i]);
-        b[i] = deltaYj[i] / h[i] - C[i][0] * h[i] - d[i] * Math.pow(h[i], 2);
-        a[i] = Y[i];
+
+    // which column to start in
+    var startCol = row - 1;
+    var stopCol = startCol + 2;
+	
+
+
+    //special cases for first and last row
+
+    if (startCol < 0) {
+        stopCol = 1;
+        startCol = 0;
     }
 
-    console.log(a);
-    console.log(b);
-    console.log(d);
+    if (stopCol > rows-1)
+        stopCol = rows-1;
 
-} else {
-    A = [
-        [1, X[0], Math.pow(X[0], 2), Math.pow(X[0], 3)],
-        [1, X[1], Math.pow(X[1], 2), Math.pow(X[1], 3)],
-        [0, 1, 2 * X[0], 3 * X[0]],
-        [0, 1, 2 * X[1], 3 * X[1]]
-    ];
 
-    B = [
-        [Y[0]],
-        [Y[1]],
-        [s0],
-        [sf]
-    ];
+    for (var col = startCol; col <= stopCol; col++) {
+        A[row][col] = Am(col-row+2 , row, h);
+    }
 
-    var Ainv = numeric.inv(A);
-    C = numeric.dot(Ainv, B);
+    B[row] = new Array();
+    B[row][0] = Bm(row, d);
 
-    console.log(C);
+
+};
+
+var Ainv = numeric.inv(A);
+C = numeric.dot(Ainv, B);
+
+//flatten result into one array mk
+var mk = [];
+mk=mk.concat.apply(mk,C);	
+
+//calculate the rest of coefficients
+var aa = [];
+var bb = [];
+var cc = [];
+var dd = [];
+
+
+for (var i = 0; i < X.length-1; i++) {
+    aa[i] = Y[i];
+	bb[i] = d[i] - (h[i]/6)*(2*mk[i]+mk[i+1]);
+	cc[i] = mk[i]/2;
+    dd[i] = (mk[i+1]-mk[i])/(6*h[i]);
 }
+
+console.log(aa);
+console.log(bb);
+console.log(cc);
+console.log(dd);
+
+
 
 
 
