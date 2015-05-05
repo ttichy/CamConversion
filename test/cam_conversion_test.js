f2ddbd49cb087521f827752741c9614d17730554
Tomas Tichy <ttichy@yahoo.com>
2015-05-04 20:21:24 -0400

    unit testing pass again after changing array format

--- test/cam_conversion_test.js
+++ test/cam_conversion_test.js
@@ -10,17 +10,15 @@ describe('Cubic spline calculations', function(){
 
 		it('Result with zero initial slopes should be [2,0,6,-4]', function (){
 			var result=cubic.CalculateCubic([1,2],[2,4],0,0);
-			result.should.have.lengthOf(4);
-
-			result.should.eql([[2],[0],[6],[-4]]);
+			// console.log(result);
+			result.should.eql([ [ 2, 0, 6, -4 ] ]);
 
 		});
 		
 		it('Result with s0=1, sf=2 should be [2,1,2,-1]', function (){
 			var result=cubic.CalculateCubic([1,2],[2,4],1,2);
-			result.should.have.lengthOf(4);
-			result.should.eql([[2],[1],[2],[-1]]);
-
+			// console.log(result);			
+			result.should.eql([ [ 2, 1, 2, -1 ] ]);
 		});
 
 	});
@@ -28,15 +26,15 @@ describe('Cubic spline calculations', function(){
 	describe('should solve points (0,2)(2,4)', function() {
 		it('Result with s0=1, sf=2 should be [2,1,2,-1]', function (){
 			var result=cubic.CalculateCubic([0,1],[2,4],1,2);
-			result.should.have.lengthOf(4);
-			result.should.eql([[2],[1],[2],[-1]]);
+			// console.log(result);
+			result.should.eql([ [ 2, 1, 2, -1 ] ]);
 
 		});
 
 		it('Result with s0=-1, sf=2 should be [2,1,6,-3]', function (){
 			var result=cubic.CalculateCubic([0,1],[2,4],-1,2);
-			result.should.have.lengthOf(4);
-			result.should.eql([[2],[-1],[6],[-3]]);
+			// console.log(result);
+			result.should.eql([ [ 2, -1, 6, -3 ] ]);
 
 		});
 
@@ -45,11 +43,14 @@ describe('Cubic spline calculations', function(){
 	describe('should solve points (0,0),(1,0),(3,2),(4,2)', function() {
 		it('Result with s0=1 and sf=0 should be [', function() {
 			var result = cubic.CalculateCubic([0,1,3,4],[0,0,2,2],0,0);
+			// console.log(result);			
 			result.should.eql(
-				[ [ 0, 0, 2 ],
-				  [ 0, 0.4285714285714286, 0.4285714285714286 ],
-				  [ -0.4285714285714286, 0.8571428571428572, -0.8571428571428572 ],
-				  [ 0.4285714285714286, -0.28571428571428575, 0.4285714285714286 ] ]
+				[ [ 0, 0, -0.4285714285714286, 0.4285714285714286 ],
+				  [ 0,
+				    0.4285714285714286,
+				    0.8571428571428572,
+				    -0.28571428571428575 ],
+				  [ 2, 0.4285714285714286, -0.8571428571428572, 0.4285714285714286 ] ]
 				  )
 		});
 	});
@@ -60,13 +61,15 @@ describe("Linear interpolation", function() {
 	describe('should solve points (0,2)(2,4)', function() {
 		it('Result should be [[2,1]]', function() {
 			var result=linear.CalculateLinear([0,2],[2,4]);
+			// console.log(result);		
 			result.should.have.lengthOf[1];
-			result.should.eql([ [ 2, 1 ] ]);
+			result.should.eql([ [ 2, 1, 0, 0 ] ]);
 		});
 	});
 	
 	describe('should solve points (0,2)(2,4),(4,4)', function() {
 		it('Result should be [ [ 2, 1 ], [ 4, 0 ] ]', function() {
+			// console.log(result);		
 			var result=linear.CalculateLinear([0,2,4],[2,4,4]);
 			result.should.eql([ [ 2, 1,0,0 ], [ 4, 0,0,0 ] ]);
 		});	
@@ -74,6 +77,7 @@ describe("Linear interpolation", function() {
 
 	describe('should solve points (0,2)(2,4),(4,5)', function() {
 		it('Result should be [ [ 2, 1 ], [ 3, 0.5 ] ]', function() {
+			// console.log(result);		
 			var result=linear.CalculateLinear([0,2,4],[2,4,5]);
 			result.should.eql([ [ 2, 1,0,0 ], [ 3, 0.5,0,0 ] ]);
 		});	
