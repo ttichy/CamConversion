fe82b69f482fc3d2bcdf156bd19692b4e543717a
Tomas Tichy <ttichy@yahoo.com>
2015-07-13 11:28:23 -0400

    fixed errors around how many cubucSegs are left and how they're calculated

--- calculateCoeffsFromCam.js
+++ calculateCoeffsFromCam.js
@@ -11,85 +11,82 @@ var cubic = require("./calculate_cubic.js");
  * @param {double} s0 [initial slope]
  * @param {double} sf [final slope]
  */
-exports.CalculateCoeffsFromCam = function(X,Y,T, s0,sf){
-	"use strict";
-
+exports.CalculateCoeffsFromCam = function(X, Y, T, s0, sf) {
 	// data checks
-	if(X.constructor != Array || Y.constructor != Array || T.constructor != Array)
+	if (X.constructor != Array || Y.constructor != Array || T.constructor != Array)
 		throw new Error("X and Y must be arrays");
 
-	if(X.length != Y.length) {
+	if (X.length != Y.length) {
 		throw new Error("Matrices must have the same length");
 	}
 
-	if(X.length==1)
-		throw new Error ("Cannot interpolate a single point");
+	if (X.length == 1)
+		throw new Error("Cannot interpolate a single point");
 
-	if(X.length-1 !=T.length)
-		throw new Error("invalid length of types array T")
+	if (X.length - 1 != T.length)
+		throw new Error("invalid length of types array T");
 
-	if(! T.every(function(el,idx,array){
+	if (!T.every(function(el, idx, array) {
 
-		return el ==1 || el ==0;
-	}))
+			return el == 1 || el == 0;
+		}))
 		throw new Error("only 1 or 0 is a valid interploation type");
 
-	var result=[];
+	var result = [];
 
-
-	var currentRow=1;
-	var cubicStart=0;
-	var initSlope=s0;
-	var finalSlope=sf;
-
-
-	debugger;
-
-	while(currentRow<=X.length)
-	{	
-		if(T[currentRow-1]==0)
-		{
+	var currentRow = 1;
+	var cubicStart = 0;
+	var initSlope = s0;
+	var finalSlope = sf;
+	while (currentRow <= X.length - 1) {
+		if (T[currentRow - 1] == 0) {
 
 			//calculate the linear segment first, cuz need final slope
-			var linRes = linear.CalculateLinear(X.slice(currentRow-1,currentRow+1), Y.slice(currentRow-1,currentRow+1));
+			var linRes = linear.CalculateLinear(X.slice(currentRow - 1, currentRow + 1), Y.slice(currentRow - 1, currentRow + 1));
 
-			finalSlope=linRes[0][1];
+			finalSlope = linRes[0][1];
 
 			//need to calculate all previous cubic rows
-			var cubicSegs=currentRow-cubicStart;
-			if(cubicSegs > 0){
-				var coeffs3=cubic.CalculateCubic(X.slice(cubicStart,cubicStart+cubicSegs),
-				 Y.slice(cubicStart,cubicStart+cubicSegs),
-				 initSlope,
-				 finalSlope);
+			var cubicSegs = currentRow - cubicStart;
+			if (cubicSegs > 1) {
+				var coeffs3 = cubic.CalculateCubic(X.slice(cubicStart, cubicStart + cubicSegs),
+					Y.slice(cubicStart, cubicStart + cubicSegs),
+					initSlope,
+					finalSlope);
 				result = result.concat(coeffs3);
 			}
 
-			
-			initSlope=linRes[0][1];
+
+			initSlope = linRes[0][1];
 
 			result = result.concat(linRes);
-			cubicStart=currentRow;
+			cubicStart = currentRow;
 		}
 
 		currentRow++;
-
 	}
 
 
 	// there may be 'leftover' cubic segments
-	var cubicSegs=currentRow-cubicStart;
-	if(cubicSegs > 0){
-		var coeffs3=cubic.CalculateCubic(X.slice(cubicStart,cubicStart+cubicSegs),
-		 Y.slice(cubicStart,cubicStart+cubicSegs),
-		 initSlope,
-		 finalSlope);
+	//current row is passed the last row now, so need to subtract one to get to actual number of segments
+	cubicSegs = currentRow - 1 - cubicStart;
+
+	// use final slope that user specified
+	finalSlope = sf;
+
+	if (cubicSegs > 0) {
+		coeffs3 = cubic.CalculateCubic(X.slice(cubicStart, cubicStart + cubicSegs + 1),
+			Y.slice(cubicStart, cubicStart + cubicSegs + 1),
+			initSlope,
+			finalSlope);
 		result = result.concat(coeffs3);
 	}
+	//console.log(result);
 	return result;
+};
 
 
-}
 
-var res1 = exports.CalculateCoeffsFromCam([0,1,3,5,7],[0,2,4,5,8],[1,1,0,1],0,0);
+var res1 = exports.CalculateCoeffsFromCam([0, 0.33333333, 0.66666666, 1], [0, .25, .75, 1], [1, 0, 1], 0, 0);
+// var res1 = exports.CalculateCoeffsFromCam([0.66666,1],[0.75,1],[1],1.5,0);
 console.log(res1);
